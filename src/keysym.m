aa5a51cef1c9ea33138202a8db3f054aa612aaaa
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2023-03-15 15:45:31 +0100

    Delete keysym.m

--- src/keysym.m
+++
@@ -1,9 +0,0 @@
-/*   _                                       _
- *  | | https://github.com/NewDawn0/logged  | |
- *  | |   / _ \   / _` |  / _` |  / _ \  / _` |
- *  | |_ | (_) | | (_| | | (_| | |  __/ | (_| |
- *  |___| \___/   \__, |  \__, |  \___|  \__,_|
- *                |___/   |___/
- *  File: keysym.m
- *  Desc: Simulate keypresses
-*/ 
